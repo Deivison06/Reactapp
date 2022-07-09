6b73c3f04bbdc2f8b63790ab52642f4f97a4b2f2
Deivison06 <santosdeivison764@gmail.com>
2022-07-09 10:02:54 -0300

    Primeiro commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Home} from './pages/home'
+import './style/global.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+)
+ 
